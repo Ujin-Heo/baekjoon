eee064c5c29bdeca7c142f16a35ea51ed7a2462e
Ujin-Heo <63441458+Ujin-Heo@users.noreply.github.com>
2024-08-06 00:36:10 +0900

    2563번 색종이

---
+++ ALPS/2563번 색종이.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+int main() {
+
+	int N = 0;
+	scanf("%d", &N);
+
+	int sheet[100][100] = { 0, };
+
+	int x = 0, y = 0;
+	for (int n = 0;n < N;n++) {
+		scanf("%d %d", &x, &y);
+		for (int i = x;i < x + 10;i++) {
+			for (int j = y;j < y + 10;j++) {
+				sheet[i][j] = 1;
+			}
+		}
+	}
+
+	int area = 0;
+	for (int i = 0;i < 100;i++) {
+		for (int j = 0;j < 100;j++) {
+			if (sheet[i][j] == 1) {
+				area++;
+			}
+		}
+	}
+	printf("%d", area);
+}
+*/
